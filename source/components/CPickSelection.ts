a42358be871058c8a2e9703f1c3238ceda300ed6
Ralph Wiedemeier <ralph@framefactory.ch>
2019-01-20 15:19:07 +0100

    add active graph support to CPickSelection

--- source/components/CPickSelection.ts
+++ source/components/CPickSelection.ts
@@ -5,11 +5,10 @@
  * License: MIT
  */
 
-import * as THREE from "three";
-
 import Node from "@ff/graph/Node";
 import Component from "@ff/graph/Component";
 import ComponentTracker from "@ff/graph/ComponentTracker";
+import Graph from "@ff/graph/Graph";
 import CSelection from "@ff/graph/components/CSelection";
 
 import Bracket from "@ff/three/Bracket";
@@ -30,7 +29,7 @@ export default class CPickSelection extends CSelection
     protected startY = 0;
 
     private _brackets = new Map<Component, Bracket>();
-    private _sceneTracker: ComponentTracker<CScene>;
+    private _sceneTracker: ComponentTracker<CScene> = null;
 
     create()
     {
@@ -38,12 +37,6 @@ export default class CPickSelection extends CSelection
 
         this.system.on<IPointerEvent>("pointer-down", this.onPointerDown, this);
         this.system.on<IPointerEvent>("pointer-up", this.onPointerUp, this);
-
-        this._sceneTracker = new ComponentTracker(this.graph.components, CScene, component => {
-            component.on<ISceneAfterRenderEvent>("after-render", this.onSceneAfterRender, this);
-        }, component => {
-            component.off<ISceneAfterRenderEvent>("after-render", this.onSceneAfterRender, this);
-        });
     }
 
     dispose()
@@ -75,6 +68,21 @@ export default class CPickSelection extends CSelection
         }
     }
 
+    protected onActiveGraph(graph: Graph)
+    {
+        if (this._sceneTracker) {
+            this._sceneTracker.dispose();
+        }
+
+        if (graph) {
+            this._sceneTracker = new ComponentTracker(graph.components, CScene, component => {
+                component.on<ISceneAfterRenderEvent>("after-render", this.onSceneAfterRender, this);
+            }, component => {
+                component.off<ISceneAfterRenderEvent>("after-render", this.onSceneAfterRender, this);
+            });
+        }
+    }
+
     protected onPointerDown(event: IPointerEvent)
     {
         if (event.isPrimary) {
@@ -88,8 +96,12 @@ export default class CPickSelection extends CSelection
         if (event.isPrimary) {
             const distance = Math.abs(this.startX - event.centerX) + Math.abs(this.startY - event.centerY);
             if (distance < 2) {
-                //this.selectNode(event.node, event.ctrlKey);
-                this.selectComponent(event.component, event.ctrlKey);
+                if (event.component) {
+                    this.selectComponent(event.component, event.ctrlKey);
+                }
+                else if (!event.ctrlKey) {
+                    this.clearSelection();
+                }
             }
         }
     }
